e8de330463566cd1905f4611063f7637a9aed0d7
idrissi <mohamed999idrissi@gmail.com>
2024-05-01 15:10:33 +0100

    Create Exemple Student Done

--- EduGate/src/main/java/BackEnd/EduGate/EduGateApplication.java
+++ EduGate/src/main/java/BackEnd/EduGate/EduGateApplication.java
@@ -1,20 +1,13 @@
 package BackEnd.EduGate;
 
-import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
+
 @SpringBootApplication
 public class EduGateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EduGateApplication.class, args);
-	}
-	@GetMapping
-    public List<String> hello(){
-		return List.of("hello","EduGate");
 	}
 }
---
+++ EduGate/src/main/java/BackEnd/EduGate/student/StudentService.java
@@ -0,0 +1,22 @@
+package BackEnd.EduGate.student;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+    
+    public List<Student> getStudentS(){
+		return List.of(
+			new Student(
+				1L,
+				"John",
+				"john.c.breckinridge@altostrat.com",
+				LocalDate.of(2000, 1, 5),
+				23
+			)
+		);
+	}
+}
