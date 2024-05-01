fc5a97ebd3b301b5feb8a59ec5163b3c96632c7c
idrissi <mohamed999idrissi@gmail.com>
2024-05-01 12:34:52 +0100

    initialisation projet EduGtae BackEnd

---
+++ EduGate/src/main/java/BackEnd/EduGate/EduGateApplication.java
@@ -0,0 +1,18 @@
+package BackEnd.EduGate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class EduGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduGateApplication.class, args);
+	}
+	@GetMapping
+    public String hello(){
+		return "hello from EduGate";
+	}
+}
